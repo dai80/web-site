bf9c3bb778dcf7f876feb104938b9b56bb1612b2
dai80 <bwtrb80@yahoo.co.jp>
2025-04-28 22:04:16 +0900

    再修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -19,20 +19,24 @@ $(function () {
 
 // swiper
 document.addEventListener("DOMContentLoaded", function () {
-    var swiper = new Swiper(".carousel", {
-        loop: false,
-        spaceBetween: 15,
-        slidesPerView: 1,
-        centeredSlides: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-    });
+    const carousel = document.querySelector('.carousel');
+    if (carousel) {
+        new Swiper(carousel, {
+            loop: false,
+            spaceBetween: 15,
+            slidesPerView: 1,
+            centeredSlides: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+        });
+    }
 });
 
+
 // アコーディオン
-$('.qa__question').on('click', function (e) {
+$('.faq__question').on('click', function (e) {
     $(this).toggleClass('open')
     var content = $(this).next();
 
